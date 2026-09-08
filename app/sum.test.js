4d38825442458e2c15009214e3954684c26861fe
“ekvt98” <evitateka@outlook.fr>
2026-09-08 20:13:04 +0200

    test correction

--- app/sum.test.js
+++ app/sum.test.js
@@ -4,5 +4,5 @@ const assert = require('node:assert')
 const {sum} = require('./sum')
 
 test('2 + 3 donne 5', () => {
-    assert.strictEqual(sum(2 + 3), 5);
+    assert.strictEqual(sum(2 , 3), 5);
 });
